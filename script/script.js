45eb4cafb444f4608b3774360bab11ef334a4772
k-quinlan <132873356+k-quinlan@users.noreply.github.com>
2023-05-10 21:12:02 +1000

    Player input statements - untested

--- script/script.js
+++ script/script.js
@@ -19,6 +19,32 @@ function getCompChoice() {
 
 console.log(getCompChoice());
 
+
+//clean player input
+function cleanPlayerInput(input) {
+    let cleanInput = toLowerCase.toString(input);
+    return cleanInput;
+}
+
+//check player input
+function checkPlayerInput(cleanInput) {
+    if (cleanInput === "rock" || "scissors" || "paper") {
+        //do nothing - check passed
+    } else {
+        alert("Incorrect Input");
+    }
+    return;
+}
+
+//get player input
+function getPlayerinput() {
+    let input = prompt("Rock, paper, scissors?");
+    let cleanInput = cleanPlayerInput(input);
+    checkPlayerinput(cleanInput);
+    return cleanInput;
+}
+
+
 //function play a round: playerselection(string - any capitalisation), computerselection
 //return: "You XXXXX: XXXX beats XXXX"
 
